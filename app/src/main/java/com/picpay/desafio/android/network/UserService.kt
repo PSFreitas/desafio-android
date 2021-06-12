5e9e9498a8badf8b4f5b26accb03116975cfe294
Paulo Sérgio <pssfjr@gmail.com>
2021-06-12 11:15:03 -0400

    #Update: Extract network setup from MainActivity

--- app/src/main/java/com/picpay/desafio/android/network/UserService.kt
+++ app/src/main/java/com/picpay/desafio/android/network/UserService.kt
@@ -1,10 +1,11 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.network
 
+import com.picpay.desafio.android.User
 import retrofit2.Call
 import retrofit2.http.GET
 
 
-interface PicPayService {
+interface UserService {
 
     @GET("users")
     fun getUsers(): Call<List<User>>
