2da5bfc421def35e44344227cb7e155368d837b9
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-06-12 10:46:23 -0400

    #Update: Adjust display list item test

--- app/src/androidTest/java/com/picpay/desafio/android/MainActivityTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/MainActivityTest.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
+import com.picpay.desafio.android.RecyclerViewMatchers.checkRecyclerViewItem
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -45,7 +45,11 @@ class MainActivityTest {
         server.start(serverPort)
 
         launchActivity<MainActivity>().apply {
-            // TODO("validate if list displays items returned by server")
+            checkRecyclerViewItem(
+                resId = R.id.recyclerView,
+                position = 0,
+                withMatcher = withId(R.id.username)
+            )
         }
 
         server.close()
