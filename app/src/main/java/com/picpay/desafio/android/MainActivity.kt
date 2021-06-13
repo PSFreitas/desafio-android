2f578fe7acaee18db21ce9cbf52d08f29729e69a
Paulo Sérgio <pssfjr@gmail.com>
2021-06-13 15:41:06 -0400

    #Update: Add Databinding to MainActivity

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -1,25 +1,22 @@
 package com.picpay.desafio.android
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.data.UserRepositoryImpl
 import com.example.data.network.api.UserClient
+import com.picpay.desafio.android.databinding.ActivityMainBinding
 import com.picpay.desafio.android.valuableobject.Status
 import com.picpay.desafio.android.viewmodel.MainViewModel
 import com.picpay.desafio.android.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var adapter: UserListAdapter
+    private val adapter: UserListAdapter = UserListAdapter()
 
     private val mainViewModel by lazy {
         ViewModelProvider(
@@ -34,14 +31,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        ).let {
+            it.viewModel = mainViewModel
+            it.lifecycleOwner = this
 
-        recyclerView = findViewById(R.id.recyclerView)
-        progressBar = findViewById(R.id.user_list_progress_bar)
-
-        adapter = UserListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+            it.recyclerView.adapter = adapter
+            it.recyclerView.layoutManager = LinearLayoutManager(this)
+        }
 
         setupObservables()
 
@@ -52,19 +51,14 @@ class MainActivity : AppCompatActivity() {
             when (it.status) {
                 Status.LOADING -> {
                     CountingIdleResource.increment()
-                    progressBar.visibility = View.VISIBLE
                 }
                 Status.SUCCESS -> {
-                    progressBar.visibility = View.GONE
                     if (it.data != null) {
                         adapter.users = it.data
                     }
                     CountingIdleResource.decrement()
                 }
                 Status.ERROR -> {
-                    progressBar.visibility = View.GONE
-                    recyclerView.visibility = View.GONE
-
                     Toast.makeText(this@MainActivity, it.exception?.message, Toast.LENGTH_SHORT)
                         .show()
                 }
