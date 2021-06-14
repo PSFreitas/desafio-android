8d980ba67734fd97430d2a77e08ac83121cfe2bd
Paulo Sérgio <pssfjr@gmail.com>
2021-06-14 19:42:33 -0400

    #Update: Rename components in main xml

--- app/src/androidTest/java/com/picpay/desafio/android/MainActivityTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/MainActivityTest.kt
@@ -60,7 +60,7 @@ class MainActivityTest {
 
         launchActivity<MainActivity>().apply {
             checkRecyclerViewItem(
-                resId = R.id.recyclerView,
+                resId = R.id.recyclerView_users,
                 position = 0,
                 withMatcher = withId(R.id.textView_username)
             )
